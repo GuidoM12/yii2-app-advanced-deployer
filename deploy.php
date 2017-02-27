8a04b35e4774c6597e38783ca8acae58ba28f925
Tej Raj Shrestha <tej.raj@bentraytech.com>
2017-02-27 12:31:02 +0545

    remove in-place

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/deployer/recipe/yii2-app-advanced.php';
 require_once __DIR__ . '/deployer/recipe/yii-configure.php';
-require_once __DIR__ . '/deployer/recipe/in-place.php';
+//require_once __DIR__ . '/deployer/recipe/in-place.php';
 
 if (!file_exists (__DIR__ . '/deployer/stage/servers.yml')) {
   die('Please create "' . __DIR__ . '/deployer/stage/servers.yml" before continuing.' . "\n");
@@ -27,5 +27,5 @@ task('deploy:configure_composer', function () {
 //after('inplace:configure', 'inplace:run_migrations');
 
 before('deploy:vendors', 'deploy:configure_composer');
-before('inplace:vendors', 'deploy:configure_composer');
+before('deploy:vendors', 'deploy:configure_composer');
 before('deploy:symlink', 'deploy:configure');
